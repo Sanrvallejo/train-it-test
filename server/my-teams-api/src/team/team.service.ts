b3b20fa432eefdeb8276c4b9e741ef06146ce441
Sanrvallejo <zanti-23@hotmail.com>
2025-06-06 17:34:26 -0500

    feat: crud methods in service

--- server/my-teams-api/src/team/team.service.ts
+++ server/my-teams-api/src/team/team.service.ts
@@ -1,6 +1,35 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { Teams } from 'generated/prisma';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class TeamService {
-    
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getAllTeams(): Promise<Teams[]> {
+    return this.prismaService.teams.findMany();
+  }
+
+  async getTeamById(id: number): Promise<Teams> {
+    return this.prismaService.teams.findUniqueOrThrow({
+      where: { id },
+    });
+  }
+
+  async createTeam(data: Teams): Promise<Teams> {
+    return this.prismaService.teams.create({ data });
+  }
+
+  async updateTeam(id: number, data: Teams): Promise<Teams> {
+    return this.prismaService.teams.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async deleteTeam(id: number): Promise<Teams> {
+    return this.prismaService.teams.delete({
+      where: { id },
+    });
+  }
 }
