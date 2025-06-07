33efc6f32c49a435b61b832fed6b4562b50020dd
Sanrvallejo <zanti-23@hotmail.com>
2025-06-07 00:52:40 -0500

    feat: routing

--- client/my-teams-client/src/app/app.routes.ts
+++ client/my-teams-client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { TeamComponent } from './components/team/team.component';
+import { EditTeamComponent } from './components/edit-team/edit-team.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: TeamComponent
+    },
+    {
+        path: 'edit',
+        component: EditTeamComponent
+    }
+];
+
