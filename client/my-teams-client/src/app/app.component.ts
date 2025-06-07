7cb0d4f9823b8713f672331342134d9539685222
Sanrvallejo <zanti-23@hotmail.com>
2025-06-06 22:05:48 -0500

    template: team list view

--- client/my-teams-client/src/app/app.component.ts
+++ client/my-teams-client/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { TeamFormComponent } from './components/team-form/team-form.component';
+import { TeamListComponent } from './components/team-list/team-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, TeamFormComponent, TeamListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
